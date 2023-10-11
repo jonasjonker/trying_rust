29577601e70c98d548b781de5f0ae700a42f269b
Jonas Jonker <jonasjonker@gmail.com>
2023-10-11 23:42:16 +0200

    chapter 2

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ description: Excercises from "the rust book"
 ---
 
 
+## Usage
+
+To run the project use:
+```
+cargo run
+```
+
 ## References
 - [Rust book](https://github.com/rust-lang/book)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Hello, world!");
+    
+    let my_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Guess my number: ");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("That's not a number...");
+                    continue;
+                },
+            };
+
+        match guess.cmp(&my_number) {
+            Ordering::Less    => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal   => {
+                println!("Bingo!");
+                break;
+            }
+        }
+    }
 }
